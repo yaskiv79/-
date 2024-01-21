acf2627c6ea423c91caa974cf4204bef4c2cebc3
Valentyna Makohonchuk <v.makohonchuk@orderry.com>
2024-01-21 17:24:08 +0200

    дз

--- dz1_loops.js
+++ dz1_loops.js
@@ -1,3 +1,21 @@
-1fffff
-tfxhjxjx
-setxyc
+//Спосіб 1
+function drawTriangle1(height, symbol) {
+    for (let i = 1; i <= height; i++) {
+        let row = '';
+        for (let j = 1; j <= i; j++) {
+            row += symbol;
+        }
+        console.log(row);
+    }
+}
+
+
+//Спосіб 2
+function drawTriangle2(height, symbol) {
+    let row = '';
+    for (let i = 1; i <= height; i++) {
+        row += symbol;
+        console.log(row);
+    }
+}
+drawTriangle2(5, '*');
