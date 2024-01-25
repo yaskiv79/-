80052c3b36d642b31f9bbf165de8feee33fba8b8
Valentyna Makohonchuk <v.makohonchuk@orderry.com>
2024-01-26 00:14:15 +0200

    2

--- dz1.js
+++ dz1.js
@@ -1,1 +1,18 @@
-1
+var myObject = {
+    property1: 'Значення1',
+    property2: 'Значення2',
+    property3: 'Значення3',
+
+    getInfo: function () {
+        for (var key in this) {
+            if (typeof this[key] !== 'function' && key !== 'getInfo') {
+                console.log(key + ': ' + this[key]);
+            }
+        }
+    }
+};
+
+
+myObject.newProperty = 'Нова властивість!';
+
+myObject.getInfo();
