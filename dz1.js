d6b515ba2eff2ece6f3c3f436b13616e09af38be
Valentyna Makohonchuk <v.makohonchuk@orderry.com>
2024-02-06 13:26:35 +0200

    2

--- dz1.js
+++ dz1.js
@@ -1,1 +1,13 @@
-1
+function myBlend(arr) {
+    const n = arr.length;
+    for (let i = n - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [arr[i], arr[j]] = [arr[j], arr[i]];
+    }
+}
+
+let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+console.log("Початковий масив:", arr);
+
+myBlend(arr);
+console.log("Після перемішування:", arr);
